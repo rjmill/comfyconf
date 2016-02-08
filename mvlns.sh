9c335738e820a70c92876ce34ec63f2f4070c49b
RJ Miller <rjmiller10@gmail.com>
2016-02-07 22:21:46 -0500

    Cleaned up mvlns and put it into a function

--- mvlns.sh
+++ mvlns.sh
@@ -2,18 +2,19 @@
 ##
 # A simple bash script to move a file into a new directory and create a symbolic
 # link from its original location to its new location
+mvlns() {
+  local file_origin=$1
+  local file_destination=$2
 
-set -e
+  mv --no-clobber -- "$file_origin" "$file_destination" || exit 1
+  if [[ -d $file_destination ]]; then
+    file_destination=$(cd -- "$file_destination" && pwd)/"$file_origin"
+    ln --symbolic -- "$file_destination" "$file_origin"
+  elif [[ -f $file_destination ]]; then
+    ln --symbolic -- "$file_destination" "$file_origin"
+  fi
 
-file_origin=$1
-file_destination=$2
+  echo "$file_origin moved to $file_destination, and symbolic link from $file_origin created"
+}
 
-mv --no-clobber $file_origin $file_destination
-if [ -d $file_destination ]; then
-  file_destination=$(cd $file_destination && pwd)/$file_origin
-  ln --symbolic $file_destination $file_origin
-elif [ -f $file_destination ]; then
-  ln --symbolic $file_destination $file_origin
-fi
-
-echo "$file_origin moved to $file_destination, and symbolic link from $file_origin created"
+mvlns $1 $2
